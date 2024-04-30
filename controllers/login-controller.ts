74f294be03b14d7a387daf1ebd3b51d9a0aa7a5d
federico722 <federicopelaezcalderon@gmail.com>
2024-04-30 07:45:50 -0500

    subo jwt

--- controllers/login-controller.ts
+++ controllers/login-controller.ts
@@ -1,45 +1,37 @@
-import jwt from "jsonwebtoken";
-import bcrypt from "bcryptjs";
+const jwt = require("jsonwebtoken");
+const bcrypt = require("bcryptjs");
+const db = require('../config/config-db.js');
 import loginRepository from "../repositories/loginRepository";
 import Login from "../Dto/loginDto";
 import { Request, Response } from "express";
 
-const secretKey = "hjgk"; 
+const secretKey = "HCA19";
 
-const auth = async (req: Request, res: Response) => {
-  try {
-    const { email, password } = req.body;
-    const result: any = await loginRepository.login(new Login(email));
+let auth = async (req: Request, res: Response) => {
+      try {
+        const {email, password} = req.body;
+        const result: any = await loginRepository.login(new Login(email));
 
-    
-    if (result[0].length > 0) {
-      const userData = result[0][0];
-
-  
-      const isPasswordValid = await bcrypt.compare(password, userData.password);
-
-      if (isPasswordValid) {
-  
-        const token = jwt.sign({ email: userData.email }, secretKey, { expiresIn: "1h" });
-
-        
-        return res.status(200).json({ 
-          status: 'Successful login',
-          token: token
+        if (result[0].length > 0){
+          const isPasswordValid = await bcrypt.compare(password, result[0][0].password);
+          const userData  =result[0][0];
+          if (isPasswordValid){
+            const token = jwt.sign({ email:userData.email}, secretKey, {expiresIn: "24h"});
+            return res.status(200).json({ 
+              status: 'Successful login',
+              token: token
+            });
+          }
+        }
+        return res.status(401).json({ 
+          status: 'Incorrect username or password'
         });
+      } catch (error) {
+        console.log(error);
       }
-    }
 
-    
-    return res.status(401).json({ 
-      status: 'Incorrect username or password'
-    });
-  } catch (error) {
-    console.log(error);
-    return res.status(500).json({ 
-      status: 'Internal Server Error'
-    });
-  }
+      
 }
+
 
 export default auth;
